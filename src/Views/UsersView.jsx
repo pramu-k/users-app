f90537db3325f33017ff52271df266213d4b4d3c
hivin <pramukaluthanthri@gmail.com>
2023-11-24 19:16:16 +0530

    UsersView styled

--- src/Views/UsersView.jsx
+++ src/Views/UsersView.jsx
@@ -12,23 +12,29 @@ const UsersView = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <h1>Title</h1>
-        <div className="">
-          <div>
-            <h1>Users</h1>
+    <div className="max-w-8xl mx-auto flex flex-col items-center">
+      <div className=" flex flex-col w-full items-center">
+        <div className="py-4 bg-cyan-500 w-full">
+          <h1 className="font-bold text-xl text-center text-white">Title</h1>
+        </div>
+        <div className="w-full py-6 px-4">
+            <h1 className="text-lg">Users</h1>
           </div>
-          <div className="border-2 flex gap-4">
+        <div>
+          
+          <div className=" py-4 flex  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-6 gap-6">
             {users.length > 0 &&
               users.map((user) => {
                 return (
-                  <Link to={'/users/'+user.id} className="border-2 border-black h-60 w-48 flex flex-col items-center p-2" key={user.id}>
-                    <div className="border-2 border-gray-200 h-auto w-full">
-                        <img className="rounded-full" src={user.avatar} alt="" />
+                  <Link to={'/users/'+user.id} className="pt-3 border-2 border-gray-200 rounded-xl h-72 w-52 items-center p-2" key={user.id}>
+                    <div className=" border-2 border-gray-400  w-full rounded-xl p-1">
+                        <img className="rounded-full w-full" src={user.avatar} alt="" />
                     </div>
-                    <h1>{user.first_name}</h1>
-                    <h2>{user.email}</h2>
+                    <div className="px-1 py-2">
+                      <h1 className="text-lg font-medium">{user.first_name}</h1>
+                      <p >{user.email}</p>
+                    </div>
+                    
                   </Link>
                 );
               })}
