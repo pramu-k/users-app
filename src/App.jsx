7cc1600dc72cd7cb5c634e2feab4f96436d61524
hivin <pramukaluthanthri@gmail.com>
2023-11-25 08:19:08 +0530

    deleted unwanted imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import UserInformationView from './Views/UserInformationView';
